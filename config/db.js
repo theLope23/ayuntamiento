f2f288001e99012c0fe24d1054bef34c99cf7e5f
theLope23 <151570997+theLope23@users.noreply.github.com>
2024-02-08 00:51:20 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config({path:'.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE,process.env.BD_USER,process.env.BD_PASSWORD,{
     host: process.env.BD_HOST,
-    port: 3306,
+    port: 3307,
     dialect: 'mysql',
     define:{
         timestamps: true
